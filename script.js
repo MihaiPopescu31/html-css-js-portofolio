5ece0ad55e131a4ff882256db656a5d8b64120ef
Mihai Popescu <mihai.gabrielpopescu1@gmail.com>
2024-08-24 22:04:18 +0300

    Adding new projects

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
+function adjustImageSizes() {
+  const containers = document.querySelectorAll("#projects .project-img");
+
+  containers.forEach(container => {
+      // Obține dimensiunile containerului
+      const containerWidth = container.parentElement.clientWidth;
+      const containerHeight = container.parentElement.clientHeight;
+
+      // Aplică dimensiunile containerului pentru imagine
+      container.style.width = `${containerWidth}px`;
+      container.style.height = `${containerHeight}px`;
+      container.style.objectFit = 'cover'; // Asigură proporțiile imaginii
+  });
+}
+
+// Apelează funcția atunci când pagina este complet încărcată
+window.addEventListener('load', adjustImageSizes);
+
+// De asemenea, apelează funcția la redimensionarea ferestrei
+window.addEventListener('resize', adjustImageSizes);
